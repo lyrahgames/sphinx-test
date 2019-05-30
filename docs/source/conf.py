938ca5a8b18ea7e800f6cc46d5ff930c8eeffde5
lyrahgames <markuspawellek@gmail.com>
2019-05-30 20:02:08 +0200

    Add Markdown example

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,8 @@ author = 'Markus Pawellek'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  'sphinx.ext.mathjax'
+  'sphinx.ext.mathjax',
+  'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
